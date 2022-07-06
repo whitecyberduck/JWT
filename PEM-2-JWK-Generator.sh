b92d30d26a17b31b42f8ec0b04007111ffb5c2dd
whitecyberduck <77878902+whitecyberduck@users.noreply.github.com>
2022-07-06 04:56:50 -0400

    Create PEM-2-JWK-Generator.sh

---
+++ PEM-2-JWK-Generator.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## Requires openssl, nodejs, jq
+
+# Checking if private key exists and, if not, generates a private-public pair
+if [ ! -f private.pem ]; then
+  openssl genrsa 2048 > private.pem
+  openssl rsa -in private.pem -pubout -out public.pem
+fi
+
+# Create JWK from public key
+if [ ! -d ./node_modules/pem-jwk ]; then
+  npm install pem-jwk
+fi
+jwk=$(./node_modules/.bin/pem-jwk public.pem)
+
+# Add additional fields
+jwk=$(echo '{"use":"sig"}' $jwk $header | jq -cs add)
+
+# Print out and save JWK to the screen
+echo '{"keys":['$jwk']}'| jq . > JWK.json
+echo -e "\n--- JWK ---"
+jq . JWK.json 
